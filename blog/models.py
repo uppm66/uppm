32ac64c258c0f69648c8c6566d76eb2e6102d4d9
uppm66 <63077943+uppm66@users.noreply.github.com>
2020-04-04 01:34:40 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -10,9 +10,10 @@ from django.db.models.signals import post_save
 from actstream.models import followers
 
 class Choix():
-    type_annonce = ('0','Atelier philo'), ('1','Boite à idée'), \
-                   ('2', 'Intendance, logistique'), ('3', 'Projets futurs'), \
+    type_annonce = (('0','Atelier philo'), ('1','Boite à idée'), 
+                   ('2', 'Intendance, logistique'), ('3', 'Projets futurs'), 
                    ('4',"Conférence mensuelle"), ('5',"RDV Mairie"),
+    )
     couleurs_annonces = {
        # 'Annonce':"#e0f7de", 'Administratif':"#dcc0de", 'Agenda':"#d4d1de", 'Entraide':"#cebacf",
        # 'Chantier':"#d1ecdc",'Jardinage':"#fcf6bd", 'Recette':"#d0f4de", 'Bricolage':"#fff2a0",
